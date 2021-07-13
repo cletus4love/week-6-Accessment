3fae0445059ba17fffebb06995fd14225d428b8a
cletus4love <85235244+cletus4love@users.noreply.github.com>
2021-07-13 21:10:53 +0100

    Add files via upload

---
+++ inlove.kt
@@ -0,0 +1,13 @@
+fun main (){ 
+println(inlove(4,4,)) 
+
+println(inlove(3,11)) 
+
+println(inlove(22,17))
+}
+fun inlove (flowerT:Int, flowerS:Int,):Boolean{
+ 
+ if(flowerT%2==0 && flowerS%2==1)
+  return true
+   return false
+   }
